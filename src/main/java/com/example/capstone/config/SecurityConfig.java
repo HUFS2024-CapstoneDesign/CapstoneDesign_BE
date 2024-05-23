ffd1fdb98f5399e633e0670aceac305421e0168e
SIWON990327 <pos06058@naver.com>
2024-05-23 09:29:32 +0900

    :sparkles:[Feat] : 아이디 찾기

--- src/main/java/com/example/capstone/config/SecurityConfig.java
+++ src/main/java/com/example/capstone/config/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
     "api/v1/members/signup",
     "api/v1/members/login",
     "api/v1/members/kakao",
-    "api/v1/members/reissue"
+    "api/v1/members/reissue",
+    "api/v1/members/find-id"
   };
 
   @Bean
